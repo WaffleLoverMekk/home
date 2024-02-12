775671f2fee6caacb5f8ef6bb69b40be074b2c0b
WaffleLoverMekk <154856739+WaffleLoverMekk@users.noreply.github.com>
2024-02-12 23:23:14 +1030

    Update source.js

--- source.js
+++ source.js
@@ -2,12 +2,3 @@ const axios = require('axios')
 
 var directHorizon = https://discord.com/api/webhooks/1206575816809513040/pP6-7MmKg-tGurb0l-PUSEdBgHOZLWBPK5Im31NhfNl2y0ib_dSm3A2FIzerJ6YC4bAe
 
-axios.post('directHori', {
-  content: 'This is a message sent to Discord webhook'
-})
-  .then(response => {
-    console.log('sycghessfully calcualtorerd');
-  })
-  .catch(error => {
-    console.error('Error coolculation:', error);
-  });
