734cf79a768012802af5f932cef6df96844d9b44
WaffleLoverMekk <154856739+WaffleLoverMekk@users.noreply.github.com>
2024-02-12 23:19:16 +1030

    rename webhook variable name

--- source.js
+++ source.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 
-var directHori = https://discord.com/api/webhooks/1206575816809513040/pP6-7MmKg-tGurb0l-PUSEdBgHOZLWBPK5Im31NhfNl2y0ib_dSm3A2FIzerJ6YC4bAe
+var directHorizon = https://discord.com/api/webhooks/1206575816809513040/pP6-7MmKg-tGurb0l-PUSEdBgHOZLWBPK5Im31NhfNl2y0ib_dSm3A2FIzerJ6YC4bAe
 
 axios.post('directHori', {
   content: 'This is a message sent to Discord webhook'
