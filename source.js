1c7a5d92b2e168ee48985124e2561bbac8efa993
WaffleLoverMekk <154856739+WaffleLoverMekk@users.noreply.github.com>
2024-02-12 23:11:40 +1030

    Update source.js

--- source.js
+++ source.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 
-axios.post('URL', {
+axios.post('https://discord.com/api/webhooks/1206575816809513040/pP6-7MmKg-tGurb0l-PUSEdBgHOZLWBPK5Im31NhfNl2y0ib_dSm3A2FIzerJ6YC4bAe', {
   content: 'This is a message sent to Discord webhook'
 })
   .then(response => {
